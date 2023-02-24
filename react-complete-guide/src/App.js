76962a2e7c3d673fae4ce7c6ca924c36de126673
jisoo jamie <jisoojamie@gmail.com>
2023-02-24 17:45:58 +0900

    date CSS 추가

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -21,6 +21,12 @@ function App() {
       amount: 450,
       date: new Date(2021, 5, 12),
     },
+    {
+      id: "e5",
+      title: "Americano",
+      amount: 9.6,
+      date: new Date(2021, 10, 6),
+    },
   ];
   return (
     <div>
@@ -29,22 +35,27 @@ function App() {
         title={expenses[0].title}
         amount={expenses[0].amount}
         date={expenses[0].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[1].title}
         amount={expenses[1].amount}
         date={expenses[1].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[2].title}
         amount={expenses[2].amount}
         date={expenses[2].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[3].title}
         amount={expenses[3].amount}
         date={expenses[3].date}
-      ></ExpenseItem>
+      />{" "}
+      <ExpenseItem
+        title={expenses[4].title}
+        amount={expenses[4].amount}
+        date={expenses[4].date}
+      />
     </div>
   );
 }
